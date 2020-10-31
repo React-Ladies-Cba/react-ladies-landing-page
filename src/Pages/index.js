2d9f8b6eb422529148037f302761914ddf17dc54
lara diaz <laradiaz215@gmail.com>
2020-10-31 14:51:43 -0300

    events components

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Main from '../components/Main/index';
+import Events from '../components/Events/index';
 
 const Home = () => {
     return (
         <>
         <Navbar/>
         <Main/>
+        <Events/>
         </>
     );
 };
---
+++ src/components/Events/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Line } from "../../Assets/images/Line.svg";
+import {
+  Event,
+  Text,
+  EventCardContainer,
+  EventCardDetail,
+  EventImg,
+  EventCardContent,
+  Li,
+  EventLink,
+  EventTopBar,
+  Title
+} from "./eventsElements";
+
+const Events = () => {
+  return (
+    <fragment>
+      <Event>
+        <Text>📅 Eventos</Text>
+        <EventCardContainer>
+          <EventTopBar>
+            <Title>Workshop React</Title>
+          </EventTopBar>
+          <EventCardDetail>
+            📅 15/08/20 🕒 19:00hs 😀 Mentora: Carolina Bustos
+          </EventCardDetail>
+          <EventImg src={Line} alt={Line} />
+          <EventCardContent>
+            <Li>CSS</Li>
+            <Li>Componente Globo</Li>
+            <Li>Reutilización de un componente</Li>
+            <Li>Binding de datos</Li>
+            <Li>Renders condicionales</Li>
+          </EventCardContent>
+          <EventLink>Leer más</EventLink>
+        </EventCardContainer>
+      </Event>
+    </fragment>
+  );
+};
+
+export default Events;
